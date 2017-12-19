9e7ab5e4d7f1f1807f3bb6fa8d3f135f5caf7b04
Javier Mejia <javier.mejia@protonmail.ch>
2017-12-18 21:04:49 -0600

    basic examples, variables and qw, added comments

--- tests/hello_world.pl
+++ tests/hello_world.pl
@@ -2,10 +2,10 @@
 use strict;
 use warnings;
 
-print "xavrb\n";
-my ($name, $last, $secondlast) = qw(John Doe Does);
+print "xavrb\n";   # basis standard io, print as in python
+my ($name, $last, $secondlast) = qw(John Doe Does); # "my" declares a lexical variable, meaning only will exist on this scope
 print "name is $name\n";
 print "lastname is $last\n";
 print "second lastname is $secondlast\n";
-my ($age, $sex, $occupation) = qw(50 male engineer);
+my ($age, $sex, $occupation) = qw(50 male engineer); #"qw", quote word, creates a list with given parameters
 print "\nthat person is $age years ols, is a $sex and is a $occupation\n"
