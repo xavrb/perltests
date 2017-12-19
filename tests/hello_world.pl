b9af08fdb5c4890bbf06e40da4ab8ff1bd42a06c
Javier Mejia <javier.mejia@protonmail.ch>
2017-12-18 20:57:06 -0600

    basic examples, variables and qw

---
+++ tests/hello_world.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+print "xavrb\n";
+my ($name, $last, $secondlast) = qw(John Doe Does);
+print "name is $name\n";
+print "lastname is $last\n";
+print "second lastname is $secondlast\n";
+my ($age, $sex, $occupation) = qw(50 male engineer);
+print "\nthat person is $age years ols, is a $sex and is a $occupation\n"
