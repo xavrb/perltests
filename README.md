df5093d3ae958162124d77a7318739bc24212ef8
Javier Mejia <javier.mejia@protonmail.ch>
2017-12-18 21:55:37 -0600

    fixed typo about collatz

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ A small set of perl scripts to help my personal learning.
 ## Covered so far
 
 * Variables and qw [[here]](./tests/hello_world.pl)  
-* Collatz series, using while statement and basic calculations, [here](./tests/collatz-series/src/collats.pl)  
+* Collatz series, using while statement and basic calculations, [here](./tests/collatz-series/src/collatz.pl)  
--- tests/hello_world.pl
+++ tests/hello_world.pl
@@ -2,10 +2,30 @@
 use strict;
 use warnings;
 
+
+#variables and qw
 print "xavrb\n";   # basis standard io, print as in python
 my ($name, $last, $secondlast) = qw(John Doe Does); # "my" declares a lexical variable, meaning only will exist on this scope
 print "name is $name\n";
 print "lastname is $last\n";
 print "second lastname is $secondlast\n";
 my ($age, $sex, $occupation) = qw(50 male engineer); #"qw", quote word, creates a list with given parameters
-print "\nthat person is $age years ols, is a $sex and is a $occupation\n"
+print "\nthat person is $age years ols, is a $sex and is a $occupation\n";
+
+#concatenation of strings
+
+my $fullname = 'mud' . "bath";
+print "The name is $fullname";
+
+
+#repetition
+my $line = '*'x100;
+my $line2 = 'Xsffdsfds-sd-fsdjfdsfjS'x200;
+print "\n$line\n";
+print "\n$line2\n";
+
+#lenght 
+my $len = length($line2);
+print "lenght of lenght2 is $len\n"
+
+
